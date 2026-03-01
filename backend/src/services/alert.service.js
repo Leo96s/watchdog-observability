5f8d38409612e916e0448eb8b1880a163925d770
Leonardo Silva <leonardosilva.00009@gmail.com>
2026-03-01 00:42:19 +0000

    fix: mais uma tentativa de resolver erro de email

--- backend/src/services/alert.service.js
+++ backend/src/services/alert.service.js
@@ -11,6 +11,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+  family: 4, 
+  debug: true,
+  logger: true,
   tls: {
     rejectUnauthorized: false,
     servername: 'smtp.gmail.com'
